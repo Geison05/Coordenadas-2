108d72f3771618d78176a12dd7c187217eeb9aed
Geison Amorim <geisonam43@gmail.com>
2022-04-12 12:19:39 -0300

    Add project files.

---
+++ Coordenadas-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            int X, Y;
+
+            //Entrada
+            string[] valores = Console.ReadLine().Split(' ');
+            X = int.Parse(valores[0]);
+            Y = int.Parse(valores[1]);
+
+            //Processamento
+            while (X != 0 && Y != 0)
+            {
+                if (X > 0 && Y > 0)
+                    Console.WriteLine("Primeiro");
+
+                else if (X < 0 && Y > 0)
+                    Console.WriteLine("Segundo");
+
+                else if (X < 0 && Y < 0)
+                    Console.WriteLine("Terceiro");
+
+                else
+                    Console.WriteLine("Quarto");
+
+                valores = Console.ReadLine().Split(' ');
+                X = int.Parse(valores[0]);
+                Y = int.Parse(valores[1]);
+
+            }
+        }
+    }
+}
